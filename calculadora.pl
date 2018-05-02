8ebae9f07cd7d18de513d9f7024b21218ca19ad7
MartinCJ08 <martin.carracoj@gmail.com>
2018-05-02 15:49:18 -0500

    Funciones aritmericas basicas

--- calculadora.pl
+++ calculadora.pl
@@ -1,4 +1,6 @@
 alv:-writeln('Que tranza').
+
+
 sum(A,B,C):-atom_number(A,Ac),
 atom_number(B,Bc),
 Resul is Ac+Bc,
@@ -14,22 +16,54 @@ send(C,selection,Resul).
 
 sum(_,_,C):-send(C,selection,0).
 
+rest(A,B,C):-atom_number(A,Ac),
+atom_number(B,Bc),
+Resul is Ac-Bc,
+send(C,selection,Resul).
 
-main:-new(D,dialog('Calculadora mk')),
+rest(A,_,C):-atom_number(A,Ac),
+Resul is Ac,
+send(C,selection,Resul).
 
-%% send(D,append,new(LblA,label(nombre,'Wazaap Uniboyaca'))),
-%% send(D,append,new(TxtA,text_item('Gimme a number'))),
-%% send(D,append,new(TxtB,text_item('Gimme another number'))),
-%% send(D,append,new(TxtC,text_item('Result'))),
-%% send(D,append,new(BtnSum,button('Sumar',message(D,'que tranza')))),
-%% send(D,append,new(BtnRest,button('Restar',message(D,'que tranza')),right,BtnSum)),
-new(TxtA,text_item('alv')),
-new(TxtB,text_item('alv')),
-new(TxtC,text_item('alv')),
+rest(_,B,C):-atom_number(B,Bc),
+Resul is Bc,
+send(C,selection,Resul).
+
+rest(_,_,C):-send(C,selection,0).
+
+mult(A,B,C):-atom_number(A,Ac),
+atom_number(B,Bc),
+Resul is Ac*Bc,
+send(C,selection,Resul).
+
+mult(_,_,C):-send(C,selection,0).
+
+
+div(A,B,C):-atom_number(A,Ac),
+atom_number(B,Bc),
+Bc>0,
+Resul is Ac/Bc,
+send(C,selection,Resul).
+%% div(A,_,C):-send(C,selection,0).
+%% div(_,_,C):-send(C,selection,0).
+
+
+
+
+
+
+
+
+
+main:-new(D,dialog('Calculadora')),
+
+new(TxtA,text_item('Gimme a number')),
+new(TxtB,text_item('Gimme another number')),
+new(TxtC,text_item('Result')),
 new(BtnSum,button('Plus',message(@prolog,sum,TxtA?selection,TxtB?selection,TxtC))),
-new(BtnRest,button('Restar',message(@prolog,alv))),
-new(BtnMul,button('Multiplicar',message(@prolog,alv))),
-new(BtnDiv,button('Dividir',message(@prolog,alv))),
+new(BtnRest,button('Restar',message(@prolog,rest,TxtA?selection,TxtB?selection,TxtC))),
+new(BtnMul,button('Multiplicar',message(@prolog,mult,TxtA?selection,TxtB?selection,TxtC))),
+new(BtnDiv,button('Dividir',message(@prolog,div,TxtA?selection,TxtB?selection,TxtC))),
 new(BtnFact,button('Factorial',message(@prolog,alv))),
 new(BtnPot,button('Potencia',message(@prolog,alv))),
 new(BtnMcd,button('MCD',message(@prolog,alv))),
